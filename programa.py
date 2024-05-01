d9dbef0b13b57c9c6f26b6f430d071871df20fd0
Edgar Moreano <moreanoe@gmail.com>
2024-05-01 00:42:41 -0500

    Asignación Módulos

--- programa.py
+++ programa.py
@@ -4,6 +4,6 @@ a = 10
 b = 5
 
 print("Suma:", operaciones.suma(a, b)) # Resultado de la suma
-# Resultado de la resta
-# Resultado de la multiplicación
+print("Suma:", operaciones.resta(a, b))# Resultado de la resta
+print("Suma:", operaciones.multiplicacion(a, b))# Resultado de la multiplicación
 print("División:", operaciones.division(a, b)) # Resultado de la división
